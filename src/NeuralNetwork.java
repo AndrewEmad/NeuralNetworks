458a65f4c2e0ddbc32f26b5003a917fdc285cfcf
Andrew Emad <andrewen2010@yahoo.com>
2017-12-11 19:57:46 +0200

    Initial Project

---
+++ src/NeuralNetwork.java
@@ -0,0 +1,19 @@
+import java.util.Vector;
+
+public class NeuralNetwork {
+	public static int[] solve(int L,int[] input,int[]output) {
+		
+		return null;
+	}
+	
+	public static int[][] getRandomWeights(int r,int c){
+		//initialize array[r][c] with random values and return it
+		return null;
+	}
+	
+	public static int[][] multiplyMatrices(int[][]matrix1,int matrix2){
+		
+		return null;
+	}
+	
+}
