d0ac74063c33360368abc0ec76b6a724545f9d75
Ahmed Majeed <aabdelmeged70@yahoo.com>
2017-12-12 23:29:02 +0200

    handle inputs

---
+++ src/Data.java
@@ -0,0 +1,26 @@
+
+public class Data {
+	public double input[];
+	public double output[];
+	
+	public Data(double[] input, double[] output) {
+		this.input = input;
+		this.output = output;
+		
+		/*int inSize=input.length;
+		int outSize=output.length;
+		this.input=new double[inSize];
+		this.output=new double[outSize];
+		
+		for(int i=0;i<inSize;i++) {
+			this.input[i]=input[i];
+		}
+		
+		for(int i=0;i<outSize;i++) {
+			this.output[i]=output[i];
+		}*/
+		
+	}
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+	public static int inputNeuransNumber;
+	public static int hiddenNeuransNumber;
+	public static int outputNeuransNumber;
+	public static int numberOfTrainingData;
+	
+	public static void main(String [] args){
+		Data[] trainingRecords = readData();
+		new NeuralNetwork().performNeuralNetwork(trainingRecords, hiddenNeuransNumber);;
+	}
+	
+	public static Data[] readData(){
+		Data[] trainingRecords = null;
+		
+		try{
+			Scanner in = new Scanner(new File("data.txt"));
+			
+			if(in.hasNext()){
+				inputNeuransNumber = in.nextInt();
+				hiddenNeuransNumber = in.nextInt();
+				outputNeuransNumber = in.nextInt();
+				numberOfTrainingData = in.nextInt();
+				
+				trainingRecords = new Data[numberOfTrainingData];
+			}
+			
+			for(int i=0;i<numberOfTrainingData;i++){
+				double[] input = new double[inputNeuransNumber];
+				double[] output = new double[outputNeuransNumber];
+				
+				for(int inputNuranIndex=0;inputNuranIndex<inputNeuransNumber;inputNuranIndex++){
+					input[inputNuranIndex] = in.nextDouble();
+				}
+				for(int outputNuranIndex=0;outputNuranIndex<outputNeuransNumber;outputNuranIndex++){
+					output[outputNuranIndex] = in.nextDouble();
+				}
+				
+				trainingRecords[i] = new Data(input,output);
+			}
+			in.close();
+		}catch(Exception e){
+			System.out.println("file not found!");
+		}
+		return trainingRecords;
+	}
+}
